c328c72ac080eab0f9fb77aa70e097ccf1864c6b
Dominic <string.reverse@proton.me>
2024-09-25 22:19:15 -0400

    Implemented GET request for client

---
+++ client.js
@@ -0,0 +1,36 @@
+import http from 'node:http';
+
+function getProjects() {
+  const options = {
+    hostname: '127.0.0.1',
+    port: 3000,
+    path: '/projects',
+    method: 'GET',
+  }
+
+  const req = http.request(options, (res) => {
+    let data = '';
+
+    // Listen for data
+    res.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    // Listen for the end of the response
+    res.on('end', () => {
+      console.log(JSON.parse(data));
+    });
+  });
+
+  req.on('error', (err) => {
+    console.error(`Request error: ${err.message}`);
+  })
+
+  req.end();
+}
+
+getProjects();
+
+
+
+
