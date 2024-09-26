1cd54d42697c75b2aa8c3fc3fd296971db7413e7
Dominic <string.reverse@proton.me>
2024-09-25 23:04:09 -0400

    Added POST support for server

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = 8000;
 
 let server = http.createServer((req, res) => {
   const inMemoryProjects = [...projects];
-  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('content-type', 'application/json');
 
   if (req.url === '/projects') {
     switch(req.method) {
@@ -14,16 +14,56 @@ let server = http.createServer((req, res) => {
         res.statusCode = 200;
         res.end(JSON.stringify(inMemoryProjects));
         break;
+      case "POST":
+        let body = '';
+
+        req.on('data', (chunk) => {
+          body += chunk.toString();
+        });
+
+        req.on('end', () => {
+          // If invalid JSON
+          if (!isJSON(body)) {
+            res.statusCode = 400;
+            res.end(JSON.stringify({message: "BAD REQUEST"}));
+            return;
+          }
+
+          const postData = JSON.parse(body);
+
+          // If duplicate
+          if (inMemoryProjects.find((proj) => proj.id === postData.id)) {
+            res.statusCode = 400;
+            res.end(JSON.stringify({message: "BAD REQUEST"}));
+            return;
+          }
+
+          // Add project
+          inMemoryProjects.push(postData);
+          res.statusCode = 201;
+          res.end(JSON.stringify(inMemoryProjects));
+        });
+        break;
     }
   } else {
     res.statusCode = 404;
     res.end(JSON.stringify({message: "NOT FOUND"}));
   }
-
 });
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+function isJSON(str) {
+  if (typeof str !== 'string') return false;
+
+  try {
+    JSON.parse(str);
+    return true;
+  } catch {
+    return false;
+  }
+}
 
 export default server;
